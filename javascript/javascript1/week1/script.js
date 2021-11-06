ea7ab836ea0a67fb026de1db4ee04ab1086dd560
Vladislav <91062615+VladSkorokhoda@users.noreply.github.com>
2021-11-06 23:26:34 +0100

    homework

---
+++ javascript/javascript1/week1/script.js
@@ -0,0 +1,65 @@
+/* My age in 2027*/ 
+/*let yearOfBerth;
+let yearFuture;
+yearOfBerth = 1985;
+yearFuture = 2027;
+const age = yearFuture - yearOfBerth;
+console.log('You will be' + age+ 'years old in' + yearFuture);*/
+
+/* Dog age*/
+/*let dogYearOfBirth;
+let dogYearFuture;
+dogYearOfBirth = 2017;
+dogYearFuture = 2027;
+const dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = false;
+if (shouldShowResultInDogYears === true)
+{
+	console.log('Your dog will be' + dogYear + ' human years old' + dogYearFuture );
+}
+else {
+	console.log('Your dog will be' + dogYear*7 + 'dog years old' + dogYearFuture);
+}*/
+
+/* House */
+/*var firstWords = ['Tesla', 'BMW', 'Mercedes', 'Vlad', 'Drugio'];
+var secondWords = ['Samsung', 'Toshiba', 'Nokia', 'Motorola', 'Vio'];
+console.log('From first array');
+var startupNameFromFirstWords;
+startupNameFromFirstWords = firstWords[Math.floor(Math.random() * 5)];
+var newStringFromFirst = 'Company name: '+startupNameFromFirstWords + ' contains '+ startupNameFromFirstWords.length + ' charcaters';
+console.log(newStringFromFirst);
+console.log('From second array');
+var startupNameFromSecondWords;
+startupNameFromSecondWords = secondWords[Math.floor(Math.random() * 5)];
+var newStringFromSecond = 'Company name: '+startupNameFromSecondWords + ' contains '+ startupNameFromSecondWords.length + ' charcaters';
+console.log(newStringFromSecond);*/
+
+/*Creative*/
+var firstWords = ['Easy', 'Awesome', 'Corporate', 'Future', 'First', 'Best', 'Decent', 'Reliable', 'Wonderful', 'Amazing'];
+var secondWords = ['Successful', 'Creative', 'Dreamy','Ambiguous', 'Heirlooms', 'First', 'Pioneers', 'First', 'Second', 'Third'];
+console.log('From first array');
+var startupNameFromFirstWords;
+startupNameFromFirstWords = firstWords[Math.floor(Math.random() * 1)];
+var newStringFromFirst = 'Company name: '+startupNameFromFirstWords + ' contains '+ startupNameFromFirstWords.length + ' charcaters';
+console.log(newStringFromFirst);
+var txt = "Easy";
+var sln = txt.length;
+console.log(txt.length);
+var x = "letters";
+console.log(x);
+console.log('From second array');
+var startupNameFromSecondWords;
+startupNameFromSecondWords = secondWords[Math.floor(Math.random() * 1)];
+var newStringFromSecond = 'Company name: '+startupNameFromSecondWords + ' contains '+ startupNameFromSecondWords.length + ' charcaters';
+console.log(newStringFromSecond);
+var txt = "Successful" /*+ 'letters'*/;
+var sln = txt.length;
+console.log(txt.length);
+var txt = 'letters';
+console.log(txt);
+//var x = "letters";
+//console.log(x);
+//console.log(newStringFromRandomNumberFromFirstWords);
+//const randomNumber = Math.floor(Math.random() * 8);
+//var sln = txt.lengthSecondWords [1];
