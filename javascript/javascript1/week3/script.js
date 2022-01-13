e6ce69c4319adc269fb1943b9e4180e90fd752de
Vlad <vlad.venmelion@gmail.com>
2022-01-13 22:01:38 +0100

    add another home work

--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -26,13 +26,13 @@ function removeName(name,names){
 }
 
 
- 80*365*24*60 = 42048000 minutes
- "Game of thrones" 3*24*60+1*60 =4380 minutes
-4380/42048000 = 0.000104 = 0.0104 % For 80 years
-"Sopranos" 3*24*60+14*60=5160 minutes
-5160/42048000 = 1.227168 = 0.0122 % For 80 years
-"The Wire" 2*24*60+12*60 = 3600 minutes
-3600/42048000 = 8.561643 = 0.0085 % For 80 years
+//  80*365*24*60 = 42048000 minutes
+//  "Game of thrones" 3*24*60+1*60 =4380 minutes
+// 4380/42048000 = 0.000104 = 0.0104 % For 80 years
+// "Sopranos" 3*24*60+14*60=5160 minutes
+// 5160/42048000 = 1.227168 = 0.0122 % For 80 years
+// "The Wire" 2*24*60+12*60 = 3600 minutes
+// 3600/42048000 = 8.561643 = 0.0085 % For 80 years
 
 const seriesDurations = [
   {
@@ -75,5 +75,3 @@ return 'Hours of :'+hoursFirstPart+ ' and minutes of :'+hoursSecondPart;
 
 
 console.log(travelTime); // 8 hours and 38 minutes
-
-
